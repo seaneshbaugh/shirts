97046fd425e30641b397cf2c770264828366ddbd
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-03-09 09:38:56 -0500

    Added more shirts. Added hover anchor links.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Shirt Collection
 
 Middleman app to build the static HTML used on [http://seaneshbaugh.com/shirts](http://seaneshbaugh.com/shirts).
+
+iso 800
+fstop 4.1
+wb incandecent
+af mode pinpoint
+ab 7 1/3
--- config.rb
+++ config.rb
@@ -30,6 +30,8 @@ require 'yaml'
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+proxy '/nolayout', '/index.html', layout: false
+
 ###
 # Helpers
 ###
